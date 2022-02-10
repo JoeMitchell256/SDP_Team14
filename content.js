07eebd4d155501b7aa6ae29317ced35f164e2967
Joseph Mitchell <70342721+JoeMitchell256@users.noreply.github.com>
2022-02-10 10:18:39 -0500

    Add image variables

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
+//Add an image of each team member to team website
+var img = document.createElement("img");
+var img2 = document.createElement("img2");
+var img3 = document.createElement("img3");
+var img4 = document.createElement("img4");
+
 
