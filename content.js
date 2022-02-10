6d45248afbf05f7bb52c0d8b02265e6d608b17ba
Joseph Mitchell <70342721+JoeMitchell256@users.noreply.github.com>
2022-02-10 10:45:01 -0500

    Add images to a block...
    This block is an html <div> tag thingy

--- content.js
+++ content.js
@@ -5,3 +5,16 @@ var img3 = document.createElement("img3");
 var img4 = document.createElement("img4");
 
 
+img.src = "Screen Shot 2022-02-10 at 10.08.41 AM.png";
+img2.src = "Screen Shot 2022-02-10 at 10.09.17 AM.png";
+img3.src = "Screen Shot 2022-02-10 at 10.09.30 AM.png";
+img4.src = "Screen Shot 2022-02-10 at 10.09.39 AM.png";
+
+var block = document.getElementById("x");
+
+
+
+block.appendChild(img);
+block.appendChild(img2);
+block.appendChild(img3);
+block.appendChild(img4);
