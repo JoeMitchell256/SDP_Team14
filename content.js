f0f0eaa5ae1cffc1713153a1eadc3b2cebfbcf87
Joseph Mitchell <70342721+JoeMitchell256@users.noreply.github.com>
2022-02-12 21:20:10 -0500

    Try fixing images not displaying issue
    Change the extension of images from .png to .PNG

--- content.js
+++ content.js
@@ -5,10 +5,10 @@ var img3 = document.createElement("img3");
 var img4 = document.createElement("img4");
 
 
-img.src = "Screen Shot 2022-02-10 at 10.08.41 AM.png";
-img2.src = "Screen Shot 2022-02-10 at 10.09.17 AM.png";
-img3.src = "Screen Shot 2022-02-10 at 10.09.30 AM.png";
-img4.src = "Screen Shot 2022-02-10 at 10.09.39 AM.png";
+img.src = "Screen Shot 2022-02-10 at 10.08.41 AM.PNG";
+img2.src = "Screen Shot 2022-02-10 at 10.09.17 AM.PNG";
+img3.src = "Screen Shot 2022-02-10 at 10.09.30 AM.PNG";
+img4.src = "Screen Shot 2022-02-10 at 10.09.39 AM.PNG";
 
 var block = document.getElementById("x");
 
